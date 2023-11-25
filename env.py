2d30ac850dd5bbf52c9b81ca5eb8e1d4cfee391d
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-11-25 21:09:28 +0900

    update env.py

--- env.py
+++ env.py
@@ -3,5 +3,4 @@
 from django.core.management.utils import get_random_secret_key
 
 with open('./config/local.py', encoding="UTF-8", mode="w") as f:
-    SECRET_KEY = get_random_secret_key()
-    f.write(f"SECRET_KEY='{SECRET_KEY}'")
+    f.write(f"SECRET_KEY='{get_random_secret_key()}'")
