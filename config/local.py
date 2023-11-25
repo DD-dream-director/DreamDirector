e8ce17c29dcf9477394a4598ac2781d0dfc5a230
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-11-25 20:56:37 +0900

    created local.py to separate SECRET_KEY variable

---
+++ config/local.py
@@ -0,0 +1,1 @@
+SECRET_KEY='d-y)@#3=gmh$kg)9mp^4i(4e-x302re(^bl_k=8_)kg_lx_^4d'
--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%3$wy(h+(_$1drvf=+-39m#pm!@j#+#gyg)mdajmv(gllpe_m'
+from local import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
