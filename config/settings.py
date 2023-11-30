8aefcf4eef1d870d2313928e8060153e65019ca8
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-11-30 19:36:01 +0900

    settings.pyのINSTALLED_APPSに今作成済みのapp(account_app等)を追加

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account_app',
+    'diagnosis_app',
+    'general_app',
 ]
 
 MIDDLEWARE = [
