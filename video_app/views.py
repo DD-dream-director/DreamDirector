904fc71315491cd4ca0801f79e91ba189e4c0a29
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-12-02 19:58:01 +0900

    video_appのrecommend_videoViewとother_videoViewをコンテキストとしてvideoの一覧が入っているオブジェクトを返すようにした

--- video_app/views.py
+++ video_app/views.py
@@ -9,7 +9,7 @@ def recommend_videosView(request):
     '''
     おすすめ動画を表示するページ用のビュー
     '''
-    videos = get_object_or_404(VideoContent)
+    videos = get_object_or_404(Video)
     return render(request, 'recommend_videos.html', context={'videos': videos})
 
 
@@ -17,8 +17,8 @@ def other_videosView(request):
     '''
     おすすめ以外の動画を表示する用のビュー
     '''
-    videos = get_object_or_404()
-    return render(request, 'other_videos.html')
+    videos = get_object_or_404(Video)
+    return render(request, 'other_videos.html',context={'videos':videos})
 
 
 def videoView(request):
