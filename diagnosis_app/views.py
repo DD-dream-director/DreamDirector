e18b506cc7116ea8b222fff73033450c001d45f8
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-12-02 20:21:00 +0900

    モデルクラスの命名変更をしたことにより生じたmigrationファイルのバグを解消した

--- diagnosis_app/views.py
+++ diagnosis_app/views.py
@@ -5,15 +5,15 @@ from .models import User_select, User
 
 def diagnosisView(request):
     if request.method == 'POST':
-        user_name = request.post.get('uer_name')
+        user_name = request.post.get('user_name')
         video_tag = request.post.get('video_tag')
 
         diagnosis = User_select(user_name=user_name, video_tag=video_tag)
         diagnosis.save()
 
-        return render(request, 'index')
+        return render(request, 'diagnosis.html')
     
-    return render(request, 'index')
+    return render(request, 'diagnosis.html')
         
 
 
