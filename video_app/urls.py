8c23a18c81cb169d42457deac14b24b18ddef9ff
taimat64 <c1219371@st.kanazawa-it.ac.jp>
2023-12-03 10:45:45 +0900

    まつやまサンプルデータ作成

--- video_app/urls.py
+++ video_app/urls.py
@@ -5,18 +5,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('recommend_videos/',recommend_videosView,name="recommend_videos"), # おすすめ動画を表示するルーティング
-    path('other_videos/',other_videosView,name="other_videos"), # おすすめ以外の動画を表示するルーティング
-    path('video/<int:original_name>/',videoView,name="video"), # 動画を一つ表示するページ
-=======
     path('recommend_videos/', recommend_videosView,
          name="recommend_videos"),  # おすすめ動画を表示するルーティング
     path('other_videos/', other_videosView,
          name="other_videos"),  # おすすめ以外の動画を表示するルーティング
     path('video/<int:video_id>', videoView, name="video"),  # 動画を一つ表示するページ
     path('post_video/', postView, name='post_video'),  # 動画を投稿するページ
->>>>>>> b2427b1bc811df64a979fba0adba8ceb333004f5
 ]
 
 if settings.DEBUG:
--- video_app/views.py
+++ video_app/views.py
@@ -21,24 +21,21 @@ def other_videosView(request):
     return render(request, '/video_app/other_videos.html', context={'videos': videos})
 
 
-<<<<<<< HEAD
-def videoView(request, original_name):
-    '''
-    動画を一つ表示する用のビュー(松山)
-    '''
-    video = get_object_or_404(Video, pk=original_name)
-    name = Video.title
-    tags = Video.tags
-    return render(request, 'video.html', {'video': video, 'name': name, 'tags': tags})
-=======
 def videoView(request, video_id: int):
     '''
     動画を一つ表示し、コメントとタグを選択する用のビュー
     '''
-    video = Video.objects.get(pk=video_id)
-    comments = Comment.objects.filter(commented_to=video)
 
-    return render(request, 'video_app/video.html')
+    video = get_object_or_404(Video, pk=video_id)
+    name = Video.title
+    tags = Video.tags
+    comments = Comment.objects.filter(commented_to=video)
+    return render(request, 'video_app/video.html', context={'video': video, 'name': name, 'tags': tags, 'comments': comments})
+
+
+    
+
+    
 
 
 def postView(request):
@@ -46,4 +43,3 @@ def postView(request):
     動画投稿用のビュー
     '''
     return render(request, 'video_app/post_video.html')
->>>>>>> b2427b1bc811df64a979fba0adba8ceb333004f5
